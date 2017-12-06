055e18a736c0463967ca6dd64207f399bfdcbc56
Manbec <manuel_bm@outlook.com>
2017-12-05 19:01:41 -0600

    Corrected api domain

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 angular.module("moviepost.config", [])
-.constant("EnvironmentConfig", {"views":"local","appdomain":"movieposts.loc","api":"http://api.movieposts.loc","cdn":"http://cdn.movieposts.loc","maintenance":false,"minifyhtml":false})
+.constant("EnvironmentConfig", {"views":"local","appdomain":"movieposts.loc","api":"http://api.moviepost.loc","cdn":"http://cdn.moviepost.loc","maintenance":false,"minifyhtml":false})
 .constant("version", "0.1.0")
 .constant("moviesApiKey", "dc58753025de257fbcecfef595460d03");
