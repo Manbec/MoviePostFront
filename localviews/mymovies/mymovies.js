585fa9bc18dfe5f464cb6047472257fd0783f2a4
Manbec <manuel_bm@outlook.com>
2017-12-06 03:30:56 -0600

    my saved movies

---
+++ localviews/mymovies/mymovies.js
@@ -0,0 +1,95 @@
+'use strict';
+var moviepost = angular.module('moviepost.mymovies', []);
+
+moviepost.config(['$routeProvider', 'EnvironmentConfig', function ($routeProvider, EnvironmentConfig) {
+
+  $routeProvider.when("/mymovies", {
+    templateUrl: EnvironmentConfig.views + 'views/mymovies/mymovies.html',
+    controller: 'MyMoviesCtrl'
+  });
+
+}]);
+
+moviepost.controller('MyMoviesCtrl', ['$scope', '$http', 'EnvironmentConfig', function ($scope, $http, EnvironmentConfig) {
+
+  // Popular movies docs: https://developers.themoviedb.org/3/movies/get-popular-movies
+  // Basic request: https://api.themoviedb.org/3/movie/550?api_key=dc58753025de257fbcecfef595460d03
+
+  $scope.myMoviesUrl = EnvironmentConfig.api + '/user/movies';
+  $scope.userFavoritesUrl = EnvironmentConfig.api + '/user/movies';
+  $scope.authToken = sessionStorage.getItem('Authorization');
+
+  if(!$scope.authToken){
+      $location.path("/login");
+      $scope.$apply();
+  }
+  
+  $scope.saveFavoriteMovie = function (movie) {
+    
+    if (!$scope.authToken) {
+      var $toastContent = $('<span>Log In to Save!</span>').add($('<a class="btn-flat toast-action" href="/login">Go</a>'));
+      Materialize.toast($toastContent, 2000);
+      return;
+    }
+
+    $http({
+      method: 'PUT',
+      url: EnvironmentConfig.api + '/user/movies',
+      data: {
+        movie: movie
+      },
+      headers: {
+        'Authorization': $scope.authToken
+      },
+      ignoreDuplicateRequest: false,
+      rejectDuplicateRequest: true,
+      requestId: 'save-favorite'
+    }).then(function successCallback(response) {
+
+      /*console.log("Save Fav Response");
+      console.log(response.data);*/
+
+    }, function errorCallback(response) {
+
+      console.log("Save Fav Error");
+      console.log(response.data);
+
+      if (response.status != 400) {
+        Materialize.toast('Error de conexión', 4000) // 4000 is the duration of the toast
+      }
+
+    });
+  }
+
+  $scope.deleteFavoriteMovie = function (movie) {
+    if(!movie.id){
+      movie.id = movie.idmovie;
+    }
+    $http({
+      method: 'DELETE',
+      url: EnvironmentConfig.api + '/user/movies/'+movie.id,
+      headers: {
+        'Authorization': $scope.authToken
+      },
+      ignoreDuplicateRequest: false,
+      rejectDuplicateRequest: true,
+      requestId: 'save-favorite'
+    }).then(function successCallback(response) {
+
+      console.log("Remove Fav Response");
+      console.log(response.data);
+
+    }, function errorCallback(response) {
+
+      console.log("Remove Fav Error");
+      console.log(response.data);
+
+      if (response.status != 400) {
+        Materialize.toast('Error de conexión', 4000) // 4000 is the duration of the toast
+      }
+
+    });
+  }
+
+
+}]);
