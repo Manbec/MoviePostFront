634de9b1caf62f1dfe337c39e7182c7af02d5f71
Manbec <manuel_bm@outlook.com>
2017-12-05 19:02:49 -0600

    login signup interaction with api

--- localviews/login/login.js
+++ localviews/login/login.js
@@ -15,22 +15,95 @@ moviepost.controller('LoginCtrl', ['$rootScope', '$scope', '$http', '$location',
   $scope.accessType = "login";
 
   angular.element(document).ready(function () {
-      if (sessionStorage.getItem('Authorization')) {
-        $location.path("/");
-        $scope.$apply();
-      }
+    if (sessionStorage.getItem('Authorization')) {
+      $location.path("/");
+      $scope.$apply();
+    }
   });
 
-  $scope.doLogin = function () {
-    // LOGIN FUNCTION HERE
-    
+  $scope.checkLogin = function () {
+
+    if ($scope.isSignUp()) {
+      $scope.doSignUp();
+    }
+    else {
+      $scope.doLogin();
+    }
   }
 
-  $scope.toggleAccessType = function(){
+  $scope.doLogin = function () {
+    console.log(EnvironmentConfig.api);
+    $http({
+      method: 'POST',
+      url: EnvironmentConfig.api + '/mplogin',
+      data: {
+        email: $scope.email,
+        password: $scope.password
+      },
+      rejectDuplicateRequest: true,
+      requestId: 'user-login'
+    }).then(function successCallback(response) {
+
+      /*console.log("Login");
+      console.log(response.data);*/
+
+      sessionStorage.setItem("Authorization", response.data);
+      $location.path("/");
+      $scope.$apply();
+
+    }, function errorCallback(response) {
+
+      /*console.log("Login Error");
+      console.log(response.data);*/
+
+      if (response.status == 400) {
+        Materialize.toast('Datos incorrectos', 2000) // 4000 is the duration of the toast
+      }
+      else {
+        Materialize.toast('Error de conexión', 4000) // 4000 is the duration of the toast
+      }
+
+    });
+  }
+
+  $scope.doSignUp = function () {
+    $http({
+      method: 'POST',
+      url: EnvironmentConfig.api + '/registration',
+      data: {
+        name: $scope.name,
+        email: $scope.email,
+        password: $scope.password,
+        password_confirmation: $scope.password_confirmation,
+      },
+      ignoreDuplicateRequest: false,
+      rejectDuplicateRequest: true,
+      requestId: 'user-register'
+    }).then(function successCallback(response) {
+
+      console.log("Register");
+      console.log(response.data);
+
+    }, function errorCallback(response) {
+
+      console.log("Register Error");
+      console.log(response.data);
+      if (response.status == 400) {
+        Materialize.toast('Datos incorrectos', 2000) // 4000 is the duration of the toast
+      }
+      else {
+        Materialize.toast('Error de conexión', 4000) // 4000 is the duration of the toast
+      }
+
+    });
+  }
+
+
+  $scope.toggleAccessType = function () {
     $scope.accessType = $scope.accessType == "login" ? "signup" : "login";
   }
 
-  $scope.isSignUp = function(){
+  $scope.isSignUp = function () {
     return $scope.accessType == "signup";
   }
 
