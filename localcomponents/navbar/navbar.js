7091b11b3e1b2aad7bdb56ab1dde02f99df2281d
Manbec <manuel_bm@outlook.com>
2017-12-05 19:03:12 -0600

    navbar getUser

--- localcomponents/navbar/navbar.js
+++ localcomponents/navbar/navbar.js
@@ -2,22 +2,52 @@
 var moviepost = angular.module('moviepost.navbar', []);
 moviepost.directive('mpnavbar', ['EnvironmentConfig', function (EnvironmentConfig) {
     return {
-        templateUrl: EnvironmentConfig.views+'components/navbar/navbar.html',
+        templateUrl: EnvironmentConfig.views + 'components/navbar/navbar.html',
         restrict: 'E',
         controller: 'NavbarController',
         controllerAs: 'mpnavbar'
     }
 }]);
 moviepost.controller('NavbarController', ['$rootScope', '$scope', '$location', 'EnvironmentConfig', function ($rootScope, $scope, $location, EnvironmentConfig) {
-    
+
     $scope.logOutMoviePost = function () {
-        
+
         window.location.href = "http://" + EnvironmentConfig.appdomain + "/logout";
 
     }
-    
-    $scope.hideLogin = function(){
-        return $location.url().indexOf("/login") >=0 || $rootScope.loggedIn();
+
+    $scope.hideLogin = function () {
+        return $location.url().indexOf("/login") >= 0 || $rootScope.loggedIn();
+    }
+
+    angular.element(document).ready(function () {
+        if (sessionStorage.getItem('Authorization') && !$rootScope.userProfile) {
+            $scope.fetchUserProfile();
+        }
+    });
+
+    $scope.fetchUserProfile = function () {
+
+        $http({
+            method: 'GET',
+            url: EnvironmentConfig.api + '/user',
+            headers: {
+                'Authorization': token
+            },
+            ignoreDuplicateRequest: true,
+            rejectDuplicateRequest: true,
+            requestId: 'fetch-user-profile'
+        }).then(function successCallback(response) {
+
+            console.log("User Profile");
+            console.log(response.data);
+
+        }, function errorCallback(response) {
+
+            /*console.log("Error User Profile");
+            console.log(response.data);*/
+            Materialize.toast('Error de conexión', 4000) // 4000 is the duration of the toast
+        });
     }
 
 }]);
